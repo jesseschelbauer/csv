880e1edbd4c59f9a30e2307ba0b1bb9d934a87d6
Jessé Schelbauer <jesseschelbauer@gmail.com>
2021-08-09 20:09:53 -0300

    Extract Car class to ner file

---
+++ csvParserRun/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CsvParserRun
+{
+    public class Car 
+    {
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+}
